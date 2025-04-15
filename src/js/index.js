1d430796e9cd380b6bcb533664c46d8089fbc001
Alex Sivets <localhost83@gmail.com>
2025-04-15 10:32:48 +0300

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+// Mobile nav
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
+
+// scrollUp
+import scrollUp from './modules/scroll-up.js';
+
+// modals
+import modals from './modules/modals.js';
+
+// AOS
+import AOS from './modules/aos.js';
---
+++ src/js/modules/aos.js
@@ -0,0 +1,9 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init({
+  once: true,
+  offset: 0,
+  duration: 1000,
+  easing: 'ease',
+  delay: 0, //100
+});
